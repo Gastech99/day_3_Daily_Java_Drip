999d3570f573bb3aa71158e72c59fd2e24f1a829
Gastech99 <diabygassamad16@gmail.com>
2025-01-14 21:55:01 +0000

    Les classes en Java

---
+++ src/pack1/Test.java
@@ -0,0 +1,11 @@
+package pack1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Test {
+    public static void main(String[] args) {
+        Point p= new Point();
+        p.x = 10;
+        p.y = 6;
+    }
+}
---
+++ src/pack2/Test2.java
@@ -0,0 +1,16 @@
+package pack2;
+
+import pack1.Point;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Point p2 = new Point();
+        /*
+        Ici x et y ne sont pas prise en compte puissequ'ils ne sont pas déclarés comme public
+        Ils sont en Friendly
+        p2.x=6;
+        p2.y=1;
+
+         */
+    }
+}
